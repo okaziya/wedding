306d12aae2aa880b4dad97186f0158ce02643ea9
Mats Blomdahl <mats.blomdahl@gmail.com>
2025-02-01 17:40:04 +0100

    Participant text corrections, for ...
    - Alexander #9
    - Carin #11
    - Ira #1
    - Yongtao #7

--- src/data/participants/alexander.ts
+++ src/data/participants/alexander.ts
@@ -4,7 +4,7 @@ export const alexanderDetails: ParticipantDetails = {
   title: "Liza's friend Alexander",
   role: "Friend of the Bride",
   description: "Alexander is an old friend of Liza, they got to know each other while she was living in Prague and have been out on many inspiring adventures together. Alexander currently lives in California and he's passionate about fine art, culture and business. When he's off work he likes to spend time with his partner Alan, go to the gym, and attend various sport activities.",
-  accommodation: "During the event weekend, you will stay in cottage 111 with Boris, Marcin and Miroslav. See map under \"Maps\" page. Looking forward to learning about your detailed travel plans as soon as available!",
+  accommodation: "During the event weekend, you will stay in cottage 111 with Boris, Marcin and Miroslav. See map under \"Maps\" page.",
   packingList: "- Umbrella\n- Rain coat\n- Swimming clothes\n- Outdoor clothes\n- Comfortable shoes",
   contact: "WhatsApp via Liza, +46 730 500 244",
   travelPlans: "We believe you will rent a car in Stockholm with Marcin, Miroslav and Boris, and then arrive at the reception in Siljansnäs at 17:00 on Friday, June 13th. Looking forward to learning about your detailed travel plans as soon as available!"
