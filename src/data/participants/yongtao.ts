013c3f7cb29bc959b633e9360d562dc151aae736
Mats Blomdahl <mats.blomdahl@gmail.com>
2025-02-01 17:42:36 +0100

    Remove umbrella recommendation, spell "raincoat" correctly

--- src/data/participants/yongtao.ts
+++ src/data/participants/yongtao.ts
@@ -7,7 +7,7 @@ export const yongtaoDetails: ParticipantDetails = {
   welcomeMessage: "Very welcome, Yongtao! We look forward to you joining our ceremony in Siljansnäs, thank you for your support, and for all our past and future events together.",
   travelPlans: "We believe you will arrive by car latest on Friday June 13th and join us at the reception at 17:00.",
   accommodation: "During the event weekend, you will stay in cottage 116 at Siljansnäs Stugby with Liza, Åke, Mats & Jennie. See map under \"Maps\" page.",
-  packingList: "- Umbrella\n- Rain coat\n- Swimming clothes\n- Outdoor clothes\n- Comfortable shoes",
+  packingList: "- Raincoat\n- Swimming clothes\n- Outdoor clothes\n- Comfortable shoes",
   contact: "WhatsApp via Mats, +46 730 567 567",
   image: "/wedding/lovable-uploads/c2c6e261-8e22-4ca8-abb0-3c9eb297db12.png"
 };
