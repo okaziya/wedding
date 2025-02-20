a646e2d4726cebf44cc2171872034cc11d434757
Mats Blomdahl <mats.blomdahl@gmail.com>
2025-02-20 19:54:53 +0100

    "domain" -> "URL" :pencil2:

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -71,7 +71,7 @@ const Blog = () => {
           <h2 className="text-2xl font-playfair mb-2">Mats & Liza getting started programming</h2>
           <time className="text-sm text-gray-500 mb-4 block">January 18th, 2025</time>
           <p className="text-lg mb-6">
-            Today we built an embryo of something for the wedding. We promise that the domain won't change.
+            Today we built an embryo of something for the wedding. We promise that the URL won't change!
           </p>
           <div className="mt-4">
             <a
