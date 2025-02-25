81cc060ef85018b2a534fe5d58cc8f8ce447d569
okaziya <liza.blomdahl@gmail.com>
2025-02-25 09:07:15 +0100

    refactor: put back ornament

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,10 +1,16 @@
+import upperOrnametImage from "/images/lm-ornament-upper.png";
+import lowerOrnametImage from "/images/lm-ornament-lower.png";
+
 const Blog = () => {
   return (
     <div className="min-h-screen pt-24 px-4 bg-wedding-background">
       <div className="max-w-[900px] mx-auto">
         <div className="flex flex-col items-center mb-12">
           <img src="/wedding/images/lm-ornament-flower.svg" alt="Decorative flower" className="w-8 h-auto mb-4" />
-          <h1 className="font-satisfy text-4xl md:text-5xl text-center text-[#571E25]">Blog</h1>
+
+          <img src={upperOrnametImage} alt="Upper ornamet image" width={328} />
+          <h1 className="font-satisfy text-4xl md:text-5xl text-center text-[#571E25] py-4">Blog</h1>
+          <img src={lowerOrnametImage} alt="Lower ornamet image" width={174} />
         </div>
 
         <div className="space-y-1">
