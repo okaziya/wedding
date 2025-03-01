a278291057354d2fdac71ce21fc7eef15e36bd6d
okaziya <liza.blomdahl@gmail.com>
2025-03-01 20:30:16 +0100

    style: :art:

--- src/pages/Vision.tsx
+++ src/pages/Vision.tsx
@@ -1,4 +1,3 @@
-
 import { Card, CardContent } from "@/components/ui/card";
 import upperOrnamentImage from "/images/lm-ornament-upper.png";
 import lowerOrnamentImage from "/images/lm-ornament-lower.png";
@@ -12,26 +11,31 @@ const Vision = () => {
           <h1 className="font-satisfy text-4xl md:text-5xl text-center text-[#571E25] py-4">Vision & Mission</h1>
           <img src={lowerOrnamentImage} alt="Lower ornament image" width={174} />
         </div>
-        
+
         <div className="bg-white px-6 md:px-16 pt-12 pb-16 mb-8 rounded-sm">
           <div className="relative">
-            <img
-              src="/wedding/images/lm-ornament-flower.svg"
-              alt="Decorative flower"
-              className="w-6 h-auto mx-auto mb-4"
-            />
-            <p className="mb-4 font-merriweather text-center">
-              <span className="font-bold">Our vision for this event</span> is to gather our closest family and friends to celebrate our deep commitment to building the future together as a couple. We celebrate Faith, Loyalty, and Unending Dedication to overcoming life's challenges until they're finished, one day at a time.
+            <p className="mb-4 font-merriweather">
+              <span className="font-bold">Our vision for this event</span> is to gather our closest family and friends
+              to celebrate our deep commitment to building the future together as a couple. We celebrate Faith, Loyalty,
+              and Unending Dedication to overcoming life's challenges until they're finished, one day at a time.
             </p>
-            
-            <p className="mb-6 font-merriweather text-center">
-              <span className="font-bold">The mission for this event</span> is to create a meet-and-greet for our close friends and family. The location is set to Mats' home village, where his ancestors lived and thrived for generations. We hope you will enjoy the peaceful atmosphere, the closeness to nature and God's blessings over all of us.
+
+            <p className="mb-6 font-merriweather ">
+              <span className="font-bold">The mission for this event</span> is to create a meet-and-greet for our close
+              friends and family. The location is set to Mats' home village, where his ancestors lived and thrived for
+              generations. We hope you will enjoy the peaceful atmosphere, the closeness to nature and God's blessings
+              over all of us.
             </p>
-            
-            <div className="max-w-md mx-auto">
-              <p className="font-bold mb-2 font-merriweather text-center">What to expect:</p>
-              <ul className="list-disc pl-6 space-y-2 font-merriweather">
-                <li className="flex items-center before:content-['•'] before:text-[#DF980C] before:text-3xl before:mr-3 before:inline-block">
+
+            <div className="max-w-md mx-auto mt-16">
+              <img
+                src="/wedding/images/lm-ornament-flower.svg"
+                alt="Decorative flower"
+                className="w-8 h-auto mx-auto mb-4"
+              />
+              <p className="font-bold mb-2 font-merriweather text-center text-[#571E25] text-2xl">What to expect:</p>
+              <ul className="list-disc pl-6 space-y-2 text-xl font-merriweather text-gray-600 mt-8">
+                <li className="flex  items-center before:content-['•'] before:text-[#DF980C] before:text-3xl before:mr-3 before:inline-block">
                   <span>Out-doorsy activities and fun</span>
                 </li>
                 <li className="flex items-center before:content-['•'] before:text-[#DF980C] before:text-3xl before:mr-3 before:inline-block">
