cc104309b35788aeebb47c6bc07e9c3b77ea58ff
okaziya <liza.blomdahl@gmail.com>
2025-03-02 21:12:38 +0100

    refactor:

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,10 +1,9 @@
 const Map = () => {
   return (
-    <div className="min-h-screen pt-24 px-4 bg-wedding-background">
+    <div className="min-h-screen pt-24 px-4 bg-wedding-background pb-12">
       <div className="max-w-6xl mx-auto">
-        <h1 className="text-4xl font-merriweather mb-12 text-center">Map</h1>
-        <div className="flex justify-center">
-          <div className="bg-white/80 backdrop-blur-sm rounded-lg p-6 shadow-lg w-full max-w-[595px]">
+        <div className="flex md:justify-center overflow-x-scroll">
+          <div className="w-full min-w-[904px]">
             {" "}
             {/* A4 width in pixels */}
             <div className="aspect-[1/1.4142]">
