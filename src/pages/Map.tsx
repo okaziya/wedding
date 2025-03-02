0e756062505e03a2fd9bef1c3fa4b9baf51c686b
okaziya <liza.blomdahl@gmail.com>
2025-03-02 21:25:10 +0100

    refactor:

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,7 +1,6 @@
-
 const Map = () => {
   return (
-    <div className="min-h-screen pt-24 px-4 bg-wedding-background pb-20">
+    <div className="min-h-screen pt-24 px-4 bg-wedding-background pb-6 md:pb-12">
       <div className="max-w-6xl mx-auto">
         <div className="flex md:justify-center md:overflow-auto overflow-x-scroll custom-scrollbar">
           <div className="w-full min-w-[904px]">
