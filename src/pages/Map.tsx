d9c0bce5b1eabeda6202e3ed1492eb76e7e9bd0f
okaziya <liza.blomdahl@gmail.com>
2025-03-05 09:00:53 +0100

    fix: add padding, fix overflow

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -2,7 +2,7 @@ const Map = () => {
   return (
     <div className="min-h-screen pt-24 px-4 bg-wedding-background pb-6 md:pb-12">
       <div className="max-w-6xl mx-auto">
-        <div className="flex md:justify-center md:overflow-auto overflow-x-scroll custom-scrollbar">
+        <div className="flex md:justify-center overflow-x-scroll custom-scrollbar">
           <div className="w-full min-w-[904px]">
             {" "}
             {/* A4 width in pixels */}
