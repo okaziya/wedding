826fad57738ace2b08e0d7e4daf0567273e62729
Mats Blomdahl <mats.blomdahl@gmail.com>
2025-03-07 16:54:43 +0100

    Travel plan updates for Alexander, Boris, Dima, Ira, Marcin and Miroslav

--- src/data/participants/boris.ts
+++ src/data/participants/boris.ts
@@ -17,8 +17,8 @@ export const borisDetails: ParticipantDetails = {
     "During the event weekend, you will stay at Siljansnäs Stugby in cottage 111 with Alexander, Marcin and Miroslav.",
   travelPlans:
     "We believe you will rent a car in Stockholm with Marcin, Miroslav and Alexander, and then arrive at the " +
-    "reception in Siljansnäs at 17:00 on Friday, June 13th. Looking forward to learning about your detailed travel " +
-    "plans as soon as they are available!",
+    "reception in Siljansnäs at 17:00 on Friday, June 13th. You will stay until Monday 16th and then go back to " +
+    "Stockholm.",
   packingList: "- Raincoat\n- Swimming clothes\n- Outdoor clothes\n- Comfortable shoes",
   contactName: "WhatsApp via Liza",
   contactNumber: "+46 730 500 244",
