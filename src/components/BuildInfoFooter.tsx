ad77c4e0f0175ac47be604dd3e9ed6a7ad179803
Mats Blomdahl <mats.blomdahl@gmail.com>
2025-03-20 14:24:40 +0100

    Shorten date from mblomdahl/wedding#1

--- src/components/BuildInfoFooter.tsx
+++ src/components/BuildInfoFooter.tsx
@@ -1,14 +1,21 @@
-import build_info from "../../public/build-info.json";
+import build_info from "@/build-info.json";
 
 export const BuildInfoFooter = () => {
+  const utcDate = new Date(build_info.BUILD_TIMESTAMP);
+  const localDayMonthStr = new Intl.DateTimeFormat("sv-SE", {
+    day: "numeric",
+    month: "numeric",
+    timeZone: "Europe/Stockholm",
+  }).format(utcDate);
+
   return (
     <footer className="items-center justify-center flex bg-wedding-background p-4">
       <span className="font-merriweather text-gray-300 text-center">
-        Revision{" "}
+        Rev.{" "}
         <a className="underline" href={build_info.COMMIT_URL} target="_blank" rel="noreferrer noopener">
           {build_info.GITHUB_SHA_SHORT}
         </a>
-        , built at {build_info.BUILD_TIMESTAMP} by{" "}
+        , built on {localDayMonthStr} by{" "}
         <a
           className="underline"
           href={"https://github.com/" + build_info.GITHUB_ACTOR}
